09f92babebd83cf6cb7b27e051d3c65b30cb63df
annijq <114050793+annijq@users.noreply.github.com>
2023-05-10 09:35:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const slayButton = document.querySelector('.js-slay');
+
+slayButton.addEventListener('click', () => {
+    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley', '_blank');
+})
